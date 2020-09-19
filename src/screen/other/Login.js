0aaa7c6f4f43a404a88b28653bbd6d5bff10ebf3
usamahbass <basalamahusamah7@gmail.com>
2020-09-19 15:19:42 +0700

    create Login UI

--- src/screen/other/Login.js
+++ src/screen/other/Login.js
@@ -1,31 +1,84 @@
 import React from 'react';
-import { View, Text, Image, TextInput, TouchableWithoutFeedback, Keyboard, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity,
+} from 'react-native';
 import { LoginStyles } from '../../styles/login/styles';
-import Icon from "react-native-vector-icons/Ionicons";
+import EyePassword from 'react-native-vector-icons/Ionicons';
+import CustomButton from '../../components/customButton';
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function Login() {
-    const [show, setShow] = React.useState(true)
-    return (
-        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()} >
-            <View style={LoginStyles.container} >
-                <Image source={require("../../assets/image/logo.png")} style={LoginStyles.logo} />
-                <Text style={LoginStyles.title} >Welcome back!</Text>
+  const [show, setShow] = React.useState(true);
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={LoginStyles.container}>
+        <Image
+          source={require('../../assets/image/logo.png')}
+          style={LoginStyles.logo}
+        />
+        <Text style={LoginStyles.title}>Welcome back!</Text>
 
-                <View>
-                    <Text style={LoginStyles.label} >Email</Text>
-                    <TextInput style={LoginStyles.input} keyboardType="email-address" placeholder="Enter your email..." />
-                </View>
+        <View>
+          <Text style={LoginStyles.label}>Email</Text>
+          <TextInput
+            style={LoginStyles.input}
+            keyboardType="email-address"
+            placeholder="Enter your email..."
+          />
+        </View>
 
-                <View>
-                    <Text style={LoginStyles.label} >Password</Text>
-                    <TextInput secureTextEntry={show} underlineColorAndroid="transparent" style={LoginStyles.input} placeholder="Enter your password..."  />
-                    <TouchableOpacity onPress={() => setShow(!show)} style={LoginStyles.eyePassword} >
-                        <Icon name={(show ? "eye-off" : "eye")} size={24} color="#D0C9D6" />
-                    </TouchableOpacity>
-                </View>
-                
-            </View>
-        </TouchableWithoutFeedback>
+        <View>
+          <Text style={LoginStyles.label}>Password</Text>
+          <TextInput
+            secureTextEntry={show}
+            underlineColorAndroid="transparent"
+            style={LoginStyles.input}
+            placeholder="Enter your password..."
+          />
+          <TouchableOpacity
+            onPress={() => setShow(!show)}
+            style={LoginStyles.eyePassword}>
+            <EyePassword name={show ? 'eye-off' : 'eye'} size={24} color="#D0C9D6" />
+          </TouchableOpacity>
+        </View>
 
-    )
+        <CustomButton
+          buttonStyle={LoginStyles.button}
+          buttonText={LoginStyles.buttonText}>
+          Login
+        </CustomButton>
+
+        <View>
+            <Text onPress={() => console.log("testing")} style={LoginStyles.forgotPassword}>Forgot Password ?</Text>
+        </View>
+
+        {/* <View style={LoginStyles.separatorContainer}>
+            <View style={LoginStyles.separator} />
+                    <View>
+                      <Text style={LoginStyles.separatorText}>Log in with social account</Text>
+                    </View>
+            <View style={LoginStyles.separator} />
+        </View> */}
+
+        <View style={LoginStyles.socialContainer} >
+            <CustomButton buttonStyle={LoginStyles.socialButton} buttonText={LoginStyles.socialText} >
+                <Icon name="facebook"/>
+                Login with Facebook
+            </CustomButton>
+
+            <CustomButton buttonStyle={{...LoginStyles.socialButton, ...LoginStyles.socialDanger}} buttonText={LoginStyles.socialText}>
+                <Icon name="google"/>
+                Login with Google
+            </CustomButton>
+        </View>
+
+      </View>
+    </TouchableWithoutFeedback>
+  );
 }
--- src/styles/login/styles.js
+++ src/styles/login/styles.js
@@ -8,11 +8,10 @@ export const LoginStyles = StyleSheet.create({
   logo: {
     width: 79,
     height: 79,
-    top: 39,
     left: 148,
   },
   title: {
-    top: 60,
+    top: 20,
     left: 131,
     fontWeight: '600',
     fontSize: 16,
@@ -20,7 +19,7 @@ export const LoginStyles = StyleSheet.create({
     lineHeight: 21,
   },
   label: {
-    top: 100,
+    top: 50,
     left: 24,
     fontSize: 14,
     fontWeight: '600',
@@ -31,7 +30,7 @@ export const LoginStyles = StyleSheet.create({
   input: {
     width: 327,
     height: 48,
-    top: 110,
+    top: 60,
     left: 24,
     marginBottom: 30,
     backgroundColor: '#FFFFFF',
@@ -45,6 +44,62 @@ export const LoginStyles = StyleSheet.create({
   },
   eyePassword: {
     left: 313,
-    top: 45,
+    bottom: 5,
   },
+  button: {
+    width: 327,
+    height: 46,
+    top: 24,
+    left: 24,
+    borderRadius: 6,
+    backgroundColor: '#384DFE',
+  },
+  buttonText: {
+    top: 10,
+    fontSize: 12,
+    lineHeight: 24,
+    fontFamily: 'Nunito-Regular',
+    color: '#ffffff',
+    textAlign: 'center',
+  },
+  forgotPassword: {
+    top: 50,
+    left: 10,
+    fontFamily: 'Nunito-Regular',
+    fontWeight: 'bold',
+    fontSize: 14,
+    lineHeight: 19,
+    color: '#B0B0B0',
+    textAlign: 'center',
+  },
+  // separatorContainer: {
+  //   flexDirection: "row",
+  //   alignItems: "center",
+  //   top: 100
+  // },
+  // separator: {
+  //   flex: 1,
+  //   height: 1,
+  //   backgroundColor: "#B0B0B0"
+  // },
+  // separatorText: {
+  //   width: "100%",
+  //   color: "#B0B0B0",
+  //   textAlign: "center"
+  // }
+  socialContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    left: 8
+  },
+  socialButton: {
+    width: 151,
+    height: 48,
+    backgroundColor: "#36589E",
+    borderRadius: 6
+  },
+  socialText: {
+    color: "#ffffff",
+    fontFamily: "Nunito-Regular"
+  }
 });
