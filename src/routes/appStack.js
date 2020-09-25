de0e4c84c99b277b836e77abbbee626335ddf2fa
albae69 <ahmadalbaihaqi69@gmail.com>
2020-09-25 14:26:23 +0700

    remove fb and google login,fix login

--- src/routes/appStack.js
+++ src/routes/appStack.js
@@ -1,25 +1,33 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from '@react-navigation/stack';
-import Login from "../screen/other/Login";
-import FirstApp from "../screen/other/firstApp";
-import HomeRoute from "./homeStack";
-import "react-native-gesture-handler";
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Login from '../screen/other/Login';
+import FirstApp from '../screen/other/firstApp';
+import HomeRoute from './homeStack';
+import 'react-native-gesture-handler';
 
-const ScreenStack = createStackNavigator() 
+const ScreenStack = createStackNavigator();
 
 const FirstStack = () => {
-    return (
-        <NavigationContainer>
-            <ScreenStack.Navigator>
-                <ScreenStack.Screen name="FirstApp" component={FirstApp} options={{headerShown: false}} />
-                <ScreenStack.Screen name="Login" component={Login} options={{
-                    headerShown: false
-                }} />
-                <ScreenStack.Screen name="Home" component={HomeRoute} />
-            </ScreenStack.Navigator>
-        </NavigationContainer>
-    )
-}
+  return (
+    <NavigationContainer>
+      <ScreenStack.Navigator>
+        <ScreenStack.Screen
+          name="FirstApp"
+          component={FirstApp}
+          options={{headerShown: false}}
+        />
+        <ScreenStack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <ScreenStack.Screen name="Home" component={HomeRoute} />
+      </ScreenStack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default FirstStack;
