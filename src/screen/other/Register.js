43b958cd10f0167e90daa9215e874fc7f0853c42
Muhammad Rizqi <muhammad-rizqi@live.com>
2020-10-05 07:04:35 +0700

    add login with social account text

--- src/screen/other/Register.js
+++ src/screen/other/Register.js
@@ -17,76 +17,81 @@ import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
+import {ScrollView} from 'react-native-gesture-handler';
 
 const Register = ({navigation}) => {
   const [show, setShow] = React.useState(true);
   return (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View style={[LoginStyles.container, {justifyContent: 'flex-start'}]}>
-        <View style={{width: wp('80%')}}>
-          <Text style={styles.headerTitle}>Register</Text>
-          <View style={styles.headerDescView}>
-            <Text style={styles.headerDesc}>Already have an account? </Text>
-            <Text
-              style={[styles.headerDesc, styles.headerDescLink]}
-              onPress={() => {
-                navigation.navigate('Login');
-              }}>
-              Login
-            </Text>
+    <ScrollView>
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View style={[LoginStyles.container, {justifyContent: 'flex-start'}]}>
+          <View style={{width: wp('80%')}}>
+            <Text style={styles.headerTitle}>Register</Text>
+            <View style={styles.flexRow}>
+              <Text style={styles.headerDesc}>Already have an account? </Text>
+              <Text
+                style={[styles.headerDesc, styles.headerDescLink]}
+                onPress={() => {
+                  navigation.navigate('Login');
+                }}>
+                Login
+              </Text>
+            </View>
           </View>
-        </View>
-        <View>
-          <Text style={LoginStyles.label}>Email</Text>
-          <TextInput
-            style={LoginStyles.input}
-            keyboardType="email-address"
-            placeholder="Enter your email..."
-          />
-        </View>
-        <View>
-          <Text style={LoginStyles.label}>Mobile Number</Text>
-          <TextInput
-            style={LoginStyles.input}
-            keyboardType="phone-pad"
-            placeholder="8123-456-7890"
-          />
-        </View>
-        <View>
-          <Text style={LoginStyles.label}>Password</Text>
-          <TextInput
-            secureTextEntry={show}
-            underlineColorAndroid="transparent"
-            style={LoginStyles.input}
-            placeholder="Enter your password..."
-          />
-          <TouchableOpacity
-            onPress={() => setShow(!show)}
-            style={LoginStyles.eyePassword}>
-            <EyePassword
-              name={show ? 'eye-off' : 'eye'}
-              size={24}
-              color="#D0C9D6"
-            />
-          </TouchableOpacity>
-        </View>
-        <CustomButton
-          onPress={() => navigation.navigate('Home')}
-          buttonStyle={LoginStyles.button}
-          buttonText={LoginStyles.buttonText}>
-          Sign Up
-        </CustomButton>
-        <View style={LoginStyles.wrapRegisterAndForgotPassword}>
           <View>
-            <Text
-              onPress={() => navigation.navigate('Login')}
-              style={LoginStyles.register}>
-              Already Registered ? Login
-            </Text>
+            <Text style={LoginStyles.label}>Email</Text>
+            <TextInput
+              style={LoginStyles.input}
+              keyboardType="email-address"
+              placeholder="Enter your email..."
+            />
+          </View>
+          <View>
+            <Text style={LoginStyles.label}>Mobile Number</Text>
+            <TextInput
+              style={LoginStyles.input}
+              keyboardType="phone-pad"
+              placeholder="8123-456-7890"
+            />
+          </View>
+          <View>
+            <Text style={LoginStyles.label}>Password</Text>
+            <TextInput
+              secureTextEntry={show}
+              underlineColorAndroid="transparent"
+              style={LoginStyles.input}
+              placeholder="Enter your password..."
+            />
+            <TouchableOpacity
+              onPress={() => setShow(!show)}
+              style={LoginStyles.eyePassword}>
+              <EyePassword
+                name={show ? 'eye-off' : 'eye'}
+                size={24}
+                color="#D0C9D6"
+              />
+            </TouchableOpacity>
+          </View>
+          <CustomButton
+            onPress={() => navigation.navigate('Login')}
+            buttonStyle={LoginStyles.button}
+            buttonText={LoginStyles.buttonText}>
+            Sign Up
+          </CustomButton>
+          <View>
+            <View style={styles.loginSocialContainer}>
+              <View style={styles.loginSocialLine} />
+              <Text
+                style={styles.loginSocialText}
+                onPress={() => navigation.navigate('Login')}>
+                Log in with social account
+              </Text>
+              <View style={styles.loginSocialLine} />
+            </View>
           </View>
         </View>
-      </View>
-    </TouchableWithoutFeedback>
+      </TouchableWithoutFeedback>
+    </ScrollView>
   );
 };
 
@@ -102,11 +107,25 @@ const styles = StyleSheet.create({
     fontSize: hp('2%'),
     color: '#B0B0B0',
   },
-  headerDescView: {
+  flexRow: {
     flexDirection: 'row',
   },
   headerDescLink: {
     fontWeight: 'bold',
     color: '#384DFE',
+  },
+  loginSocialText: {
+    color: '#B0B0B0',
+    flex: 3,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  loginSocialLine: {flex: 1, height: 2, backgroundColor: '#B0B0B0'},
+  loginSocialContainer: {
+    marginTop: 50,
+    width: wp('90%'),
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
