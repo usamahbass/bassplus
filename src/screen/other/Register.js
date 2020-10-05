8af021b32c2916bc7b92db16cc450c8c76a5a4a8
Muhammad Rizqi <muhammad-rizqi@live.com>
2020-10-05 07:34:10 +0700

    Add Login with Socmed

--- src/screen/other/Register.js
+++ src/screen/other/Register.js
@@ -17,81 +17,96 @@ import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
-import {ScrollView} from 'react-native-gesture-handler';
 
 const Register = ({navigation}) => {
   const [show, setShow] = React.useState(true);
   return (
-    <ScrollView>
-      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-        <View style={[LoginStyles.container, {justifyContent: 'flex-start'}]}>
-          <View style={{width: wp('80%')}}>
-            <Text style={styles.headerTitle}>Register</Text>
-            <View style={styles.flexRow}>
-              <Text style={styles.headerDesc}>Already have an account? </Text>
-              <Text
-                style={[styles.headerDesc, styles.headerDescLink]}
-                onPress={() => {
-                  navigation.navigate('Login');
-                }}>
-                Login
-              </Text>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={[LoginStyles.container, {justifyContent: 'flex-start'}]}>
+        <View style={{width: wp('80%')}}>
+          <Text style={styles.headerTitle}>Register</Text>
+          <View style={styles.flexRow}>
+            <Text style={styles.headerDesc}>Already have an account? </Text>
+            <Text
+              style={[styles.headerDesc, styles.headerDescLink]}
+              onPress={() => {
+                navigation.navigate('Login');
+              }}>
+              Login
+            </Text>
+          </View>
+        </View>
+        <View>
+          <Text style={LoginStyles.label}>Email</Text>
+          <TextInput
+            style={LoginStyles.input}
+            keyboardType="email-address"
+            placeholder="Enter your email..."
+          />
+        </View>
+        <View>
+          <Text style={LoginStyles.label}>Mobile Number</Text>
+          <TextInput
+            style={LoginStyles.input}
+            keyboardType="phone-pad"
+            placeholder="8123-456-7890"
+          />
+        </View>
+        <View>
+          <Text style={LoginStyles.label}>Password</Text>
+          <TextInput
+            secureTextEntry={show}
+            underlineColorAndroid="transparent"
+            style={LoginStyles.input}
+            placeholder="Enter your password..."
+          />
+          <TouchableOpacity
+            onPress={() => setShow(!show)}
+            style={LoginStyles.eyePassword}>
+            <EyePassword
+              name={show ? 'eye-off' : 'eye'}
+              size={24}
+              color="#D0C9D6"
+            />
+          </TouchableOpacity>
+        </View>
+        <CustomButton
+          onPress={() => navigation.navigate('Login')}
+          buttonStyle={LoginStyles.button}
+          buttonText={LoginStyles.buttonText}>
+          Sign Up
+        </CustomButton>
+        <View>
+          <View style={styles.loginSocialContainer}>
+            <View style={styles.loginSocialLine} />
+            <Text
+              style={styles.loginSocialText}
+              onPress={() => navigation.navigate('Login')}>
+              Log in with social account
+            </Text>
+            <View style={styles.loginSocialLine} />
+          </View>
+          <View style={styles.flexRow}>
+            <View style={{flex: 1, marginRight: 8}}>
+              <Icon.Button name="facebook" backgroundColor="#3b5998">
+                Facebook
+              </Icon.Button>
             </View>
-          </View>
-          <View>
-            <Text style={LoginStyles.label}>Email</Text>
-            <TextInput
-              style={LoginStyles.input}
-              keyboardType="email-address"
-              placeholder="Enter your email..."
-            />
-          </View>
-          <View>
-            <Text style={LoginStyles.label}>Mobile Number</Text>
-            <TextInput
-              style={LoginStyles.input}
-              keyboardType="phone-pad"
-              placeholder="8123-456-7890"
-            />
-          </View>
-          <View>
-            <Text style={LoginStyles.label}>Password</Text>
-            <TextInput
-              secureTextEntry={show}
-              underlineColorAndroid="transparent"
-              style={LoginStyles.input}
-              placeholder="Enter your password..."
-            />
-            <TouchableOpacity
-              onPress={() => setShow(!show)}
-              style={LoginStyles.eyePassword}>
-              <EyePassword
-                name={show ? 'eye-off' : 'eye'}
-                size={24}
-                color="#D0C9D6"
-              />
-            </TouchableOpacity>
-          </View>
-          <CustomButton
-            onPress={() => navigation.navigate('Login')}
-            buttonStyle={LoginStyles.button}
-            buttonText={LoginStyles.buttonText}>
-            Sign Up
-          </CustomButton>
-          <View>
-            <View style={styles.loginSocialContainer}>
-              <View style={styles.loginSocialLine} />
-              <Text
-                style={styles.loginSocialText}
-                onPress={() => navigation.navigate('Login')}>
-                Log in with social account
-              </Text>
-              <View style={styles.loginSocialLine} />
+            <View style={{flex: 1}}>
+              <Icon.Button name="google" backgroundColor="#EB4132">
+                Google
+              </Icon.Button>
             </View>
           </View>
         </View>
-      </TouchableWithoutFeedback>
-    </ScrollView>
+        <View style={styles.termContainer}>
+          <Text style={styles.termText}>
+            By clicking sign up you are agreeing to the Term of use and the
+            Privacy policy
+          </Text>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -115,17 +130,28 @@ const styles = StyleSheet.create({
     color: '#384DFE',
   },
   loginSocialText: {
+    fontFamily: 'Nunito-Regular',
     color: '#B0B0B0',
     flex: 3,
     textAlign: 'center',
-    fontWeight: 'bold',
+    fontWeight: '600',
   },
-  loginSocialLine: {flex: 1, height: 2, backgroundColor: '#B0B0B0'},
+  loginSocialLine: {flex: 1, height: 1, backgroundColor: '#B0B0B0'},
   loginSocialContainer: {
     marginTop: 50,
+    marginBottom: 20,
     width: wp('90%'),
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  termContainer: {
+    marginVertical: 30,
+    width: wp('80%'),
+  },
+  termText: {
+    fontFamily: 'Nunito-Regular',
+    color: '#B0B0B0',
+    textAlign: 'center',
   },
 });
